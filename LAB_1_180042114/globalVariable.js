1d7aa30a3e79163f5ecb18244d64c5d6e0a97acb
moshiurrafialvi <moshiurrafi@iut-dhaka.edu>
2021-05-07 00:12:41 +0600

    Completed globalVariable and Module Export

---
+++ LAB_1_180042114/globalVariable.js
@@ -0,0 +1,20 @@
+//__dirname=path to the current directory
+
+const currentDir = __dirname;
+console.log(currentDir);
+
+//__filename=filename
+
+console.log(__filename);
+
+//require=function to use modules
+const fs = require("fs");
+console.log(fs);
+
+//module=info about current module
+
+console.log(module);
+
+//process=info about where the program is being executed
+
+console.log(process);
