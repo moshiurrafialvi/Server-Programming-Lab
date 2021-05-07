b2d438f6252e39c035478708b18ee562683212af
moshiurrafialvi <moshiurrafi@iut-dhaka.edu>
2021-05-07 12:06:57 +0600

    Fixed http-modules

--- LAB_1_180042114/http-modules.js
+++ LAB_1_180042114/http-modules.js
@@ -1,23 +1,20 @@
+const http = require("http");
 
+const server = http.createServer((req, res) => {
+  // res.write("<h1>This is a test server</h1>Hello");
 
-const http = require ('http')
-
-const server = http.createServer((req,res)=>{
-   
-    // res.write("<h1>This is a test server</h1>Hello");
-  
-    if(req.url=='/'){
-        res.write("<h1>This is base Url</h1>");
-        res.end();
-    }
-    else if(req.url=='/home'){
-        res.write("<h1>This is home page</h1>");
-        res.end();
-    }
-    else{
-        res.write("<h1>This is non-existing</h1><br><a href = '/'>Go back to base URL</a>");
-        res.end();
-    }
+  if (req.url == "/") {
+    res.write("<h1>This is base Url</h1>");
+    res.end();
+  } else if (req.url == "/home") {
+    res.write("<h1>This is home page</h1>");
+    res.end();
+  } else {
+    res.write(
+      "<h1>This Page is non-existing</h1><br><a href = '/'>Go back to base URL</a>"
+    );
+    res.end();
+  }
 });
 
-module.exports={server};
+module.exports = { server };
