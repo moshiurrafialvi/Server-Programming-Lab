e7a1c07a504456bfb7479211d22a142cc3d4be76
moshiurrafialvi <moshiurrafi@iut-dhaka.edu>
2021-07-02 07:32:01 +0600

    Template Complete

--- Lab_2+3_Revisited/routes/index.routes.js
+++ Lab_2+3_Revisited/routes/index.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("dashboard.ejs");
+  res.render("users/register-v2.ejs");
 });
 
 module.exports = router;
