28b254aee04754b477c815bb51589c76f629d30e
moshiurrafialvi <moshiurrafi@iut-dhaka.edu>
2021-07-02 07:21:14 +0600

    Template Working

--- Lab_2+3_Revisited/app.js
+++ Lab_2+3_Revisited/app.js
@@ -4,6 +4,9 @@ const app = express();
 //Static Resources
 app.use(express.static("public"));
 
+//view Engine
+app.set("view engine", "ejs");
+
 //Routes
 const indexRoutes = require("./routes/index.routes");
 app.use(indexRoutes);
