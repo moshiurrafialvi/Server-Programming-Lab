e977a93a44801745e550a64dafd9bee173d2307c
moshiurrafialvi <moshiurrafi@iut-dhaka.edu>
2021-07-02 14:00:44 +0600

    authentication dashboard

--- Lab_2+3_Revisited/routes/index.routes.js
+++ Lab_2+3_Revisited/routes/index.routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const ensureAuthenticated = require("./../middleware/auth.middleware");
 
 router.get("/", (req, res) => {
   res.render("welcome.ejs");
 });
 
-router.get("/dashboard", (req, res) => {
+router.get("/dashboard", ensureAuthenticated, (req, res) => {
   res.render("dashboard.ejs");
 });
 
