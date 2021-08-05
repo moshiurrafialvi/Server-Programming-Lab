f90f1fa89e607f064f3eb7f2f285f48ecad2ff64
moshiurrafialvi <moshiurrafi@iut-dhaka.edu>
2021-08-05 14:45:49 +0600

    Implemented Routes

---
+++ LAB_4_180042114/routes/programmingContest.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  ensureAuthenticated,
+  addUserData,
+} = require("../middlewares/auth.middleware");
+
+const {
+  getPC,
+  postPC,
+  getPCList,
+  deletePC,
+  paymentDonePC,
+  selectPC,
+} = require("../controllers/programmingContest.controller");
+
+outer.get("/register", ensureAuthenticated, addUserData, getPC);
+router.post("/register", ensureAuthenticated, addUserData, postPC);
+
+router.get("/list", ensureAuthenticated, addUserData, getPCList);
+router.get("/delete/:id", ensureAuthenticated, addUserData, deletePC);
+router.get("/paymentDone/:id", ensureAuthenticated, addUserData, paymentDonePC);
+router.get("/select/:id", ensureAuthenticated, addUserData, selectPC);
