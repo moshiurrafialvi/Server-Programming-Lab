0479460a37bd4d3205ec9f2bb43a4fc5486bfbc3
moshiurrafialvi <moshiurrafi@iut-dhaka.edu>
2021-08-06 05:49:15 +0600

    MathOlympiad User saved on DB

--- LAB_4_180042114/controllers/mathOlympiad.controller.js
+++ LAB_4_180042114/controllers/mathOlympiad.controller.js
@@ -6,6 +6,60 @@ const getMO = (req, res) => {
 
 const postMO = (req, res) => {
   res.render("math-olympiad/list.ejs");
+  const { name, category, contact, email, institution, tshirt } = req.body;
+  console.log(name);
+  console.log(category);
+  console.log(contact);
+  console.log(email);
+  console.log(institution);
+  console.log(tshirt);
+
+  let registrationFee = 0;
+  if (category == "School") {
+    registrationFee = 250;
+  } else if (category == "College") {
+    registrationFee = 400;
+  } else {
+    registrationFee = 500;
+  }
+
+  const total = registrationFee;
+  const paid = 0;
+  const selected = false;
+
+  let error = "";
+
+  MathOlympiad.findOne({ name: name, contact: contact }).then((participant) => {
+    if (participant) {
+      error = "Participant with this name and contact number already exists!";
+      req.flash("error", error);
+      res.redirect("/MathOlympiad/register");
+    } else {
+      const participant = new MathOlympiad({
+        name,
+        category,
+        contact,
+        email,
+        institution,
+        paid,
+        total,
+        selected,
+        tshirt,
+      });
+      participant
+        .save()
+        .then(() => {
+          error = "Participant has been registered successfully!";
+          req.flash("error", error);
+          res.redirect("/MathOlympiad/register");
+        })
+        .catch(() => {
+          error = "An unexpected error occured while registering participant";
+          req.flash("error", error);
+          res.redirect("/MathOlympiad/register");
+        });
+    }
+  });
 };
 
 const deleteMO = (req, res) => {
