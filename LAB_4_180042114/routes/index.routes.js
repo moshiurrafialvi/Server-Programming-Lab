35c4496f423307c990c8711ce988f75de83b1d8c
moshiurrafialvi <moshiurrafi@iut-dhaka.edu>
2021-08-05 13:41:09 +0600

    Initial Setup Done

---
+++ LAB_4_180042114/routes/index.routes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const {
+  ensureAuthenticated,
+  addUserData,
+} = require("./../middleware/auth.middleware");
+
+router.get("/", (req, res) => {
+  res.render("welcome.ejs");
+});
+
+router.get("/dashboard", ensureAuthenticated, addUserData, (req, res) => {
+  res.render("dashboard.ejs");
+});
+
+module.exports = router;
