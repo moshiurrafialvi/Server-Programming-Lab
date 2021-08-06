fe1dcafd4595adc2737945ccd8aeeaade8b70e0c
moshiurrafialvi <moshiurrafi@iut-dhaka.edu>
2021-08-06 06:07:32 +0600

    Participant List Shown for MO

--- LAB_4_180042114/controllers/mathOlympiad.controller.js
+++ LAB_4_180042114/controllers/mathOlympiad.controller.js
@@ -66,7 +66,23 @@ const deleteMO = (req, res) => {
   res.render("math-olympiad/list.ejs");
 };
 const getMOList = (req, res) => {
-  res.render("math-olympiad/list.ejs");
+  let all_participant = [];
+  let error = "";
+  MathOlympiad.find()
+    .then((data) => {
+      all_participant = data;
+      res.render("math-olympiad/list.ejs", {
+        error: req.flash("error"),
+        participants: all_participant,
+      });
+    })
+    .catch(() => {
+      error = "Failed to retrieve data!";
+      res.render("math-olympiad/list.ejs", {
+        error: req.flash("error", error),
+        participants: all_participant,
+      });
+    });
 };
 
 module.exports = { getMO, postMO, deleteMO, getMOList };
